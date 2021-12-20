526f797430caf27f5e4cef993239d8093c375f90
Claudio Frey <frey.claudio.ch@icloud.com>
2021-12-20 17:01:01 +0100

    Empty Klassen deactivate

--- javascript/main.js
+++ javascript/main.js
@@ -9,14 +9,29 @@ $.get("http://sandbox.gibm.ch/berufe.php", function (data) {
 });
 
 $("#beruf").change(function (event) {
-    console.log("klasse.select:",$(this).val())
+    console.log("klasse.select:", $(this).val())
     $.get("http://sandbox.gibm.ch/klassen.php?beruf_id=" + $(this).val(), function (data) {
-    console.log(data);
-    data.forEach(klasse => {
-        if (klasse.klasse_name == '');
+        console.log(data);
+
+        if (data.length == 0) {
+            $("#klasse")
+                .html('')
+                .append('<option value="-1">Keine Klassen gefunden</option>')
+                .prop("disabled", true);
+            return
+        }
 
         $("#klasse")
-            .append('<option value="' + klasse.klasse_id + '">' + klasse.klasse_name + '</option>')
+            .html('')
+            .append('<option value="0">Klasse wählen</option>')
+            .prop("disabled", false);
+
+
+        data.forEach(klasse => {
+            if (klasse.klasse_name == '');
+
+            $("#klasse")
+                .append('<option value="' + klasse.klasse_id + '">' + klasse.klasse_name + '</option>')
+        });
     });
-});
 })
